949fa0fb015cca19c27b3f31679ce994461f3015
Ali Mujthaba <a.mujthaba321@gmail.com>
2018-03-10 11:13:08 +0500

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function(){
+
+    return [
+        {title: "Harry Potter"},
+        {title: "Hunger Games"},
+        {title: "A Series of Unfortunate Events"},
+        {title: "Sherlock Holmes"}
+    ]
+
+}
